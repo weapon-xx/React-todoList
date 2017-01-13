dd7cde430df2088ff8f4b5113f505c40ee83c381
weapon-xx <88180569@qq.com>
2017-01-13 10:19:05 +0800

    update

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const koa = require('koa')
 const app = koa()
+const router = require('koa-router')()
 
-app.use(function *(){
+router.get('/',function *(next){
   this.body = 'hello world'
 })
 
+app.use(router.routes())
+   .use(router.allowedMethods());
+
 app.listen(3000,()=>{
-  console.log(`God bless love ...`);
-  console.log(`Web server is running on 3000`);
+  console.log(`God bless love ...`)
+  console.log(`Web server is running on 3000`)
 })
